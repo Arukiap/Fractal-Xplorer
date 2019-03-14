f9a5f437dc409c1d66db10f82e12e901fa82ad5b
Ruben Carvalho <33361409+Arukiap@users.noreply.github.com>
2019-03-14 18:04:08 +0000

    add libs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#define SDL_MAIN_HANDLED //https://stackoverflow.com/questions/32342285/undefined-reference-to-winmain16-c-sdl-2/32343111#32343111
+#include <SDL2/SDL.h>
+
 
 void display(){
+    SDL_Init(SDL_INIT_EVERYTHING);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char* argv[]){
     std::cout << "Hello world!" << std::endl;
+    display();
     return 0;
 }
